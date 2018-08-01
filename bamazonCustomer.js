0cf8b4207af695d00374fbcd2eb884192f2820ab
Angela Andrews <aandrews17@yahoo.com>
2018-07-31 22:59:10 -0400

    My 1st promise

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,33 +25,76 @@ function start() {
   });
 
 };
-// ID | item | price
-// ask which item they'd like to buy (inquirer) add function to call select 
-var question = [
-  {
-    type: 'input',
-    name: 'item_id',
-    message: "What is the ID of the item you'd like to purchase? \n"
-  }
-  
-];
+// ask which item they'd like to buy (inquirer) add function to call select * on item_id
+
 function likeToBuy(){
+  var question = [
+    {
+      type: 'input',
+      name: 'item_id',
+      message: "What is the ID of the item you'd like to purchase? \n"
+    }
+    
+  ];
       inquirer.prompt(question).then(answers => {
         //console.log(JSON.stringify(answers, null, '  '));
-        //console.log(`ID: ${answers.item_id}`)
         itemNumber = answers.item_id;
-         //console.log(`ID ${itemNumber}`);
-         //function call to query this item passing in item_id
-         listSingleItem(itemNumber)
-});
-}
+        //function call to query this item passing in item_id
+        //listSingleItem(itemNumber)
+})
+ .then(function(){
+    //when the prompt is answered, query the db
+    connection.query("SELECT * FROM products WHERE item_id =?", [itemNumber], (err, res)=>{
+    //console.log(res);
+    for(var i=0; i< res.length; i++){
+    console.log(` Item ID: ${res[i].item_id} || ${res[i].product_name} || ${res[i].price}\n\n`);
+    };
+    
+  });
+   
+ });
+
+
+}// maybe a then here to run howMany()????
+//list out item_id. use that to  ask how many units they'd like to purchase
 function listSingleItem(id){
   var query = connection.query("SELECT * FROM products WHERE item_id =?", [id], (err, res)=>{
     //console.log(res);
     for(var i=0; i< res.length; i++){
-    console.log(` Item ID: ${res[i].item_id} || ${res[i].product_name} || ${res[i].price}`)
+    console.log(` Item ID: ${res[i].item_id} || ${res[i].product_name} || ${res[i].price}\n\n`);
     };
   });
+  //maybe call howMany() and pass in the id for another requery to check quantity
+  
 }
-//list out item_id. use that to  ask how many untits they'd like to purchase
-// 
+// howMany(); //having it here makes it call before the intial select.
+// function asking how many units they'd like to buy
+function howMany(){
+  //inquirer
+  var question = [
+    {
+      type: 'input',
+      name: 'quantity',
+      message: "How many would you like to buy? \n"
+    }
+  ];
+    inquirer.prompt(question).then(answers => {
+      //console.log(JSON.stringify(answers, null, '  '));
+      var qty = answers.quantity;
+      console.log(`Quantity: ${qty}`);
+      //function call to query this item passing in item_id
+      //listSingleItem(id);
+});
+  //take the number and re query selectSingleItem passing 
+  
+};
+
+// function quantityCheck(x){
+//   //requery the item and check the quantity
+
+//   /*if (stock_quantity >0){
+
+//   }
+//   */
+
+// };
