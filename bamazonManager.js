e8c9fc10842dc0f80f81094cebbbd0a865ed0b2e
Angela Andrews <aandrews17@yahoo.com>
2018-08-03 23:52:41 -0400

    building out manager view

--- bamazonManager.js
+++ bamazonManager.js
@@ -8,41 +8,67 @@ connection.connect(function (err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
-  
-  managerView();
+
+  start();
 });
 
+function start() {
 
-var inquirer = require('inquirer');
-var array = [
-  "View Products for Sale",
-  "View Low Inventory",
-  "Add to Inventory",
-  "Add New Product"
-];
-
-function managerView() {}
-inquirer.prompt([{
-    type: 'list',
-    name: 'manage',
-    message: 'Manager View Menu: Pick a task.',
-    choices: array,
-    filter: function (val) {
-      switch (val) {
-        case "View Products for Sale":
-          return forSale;
-        case "View Low Inventory":
-          return lowInv;
-        case "Add to Inventory":
-          return addInv;
-        case "Add New Product":
-          return addProduct;
-        default:
-          console.log(`Error detected.`)
+  managerView()
+    .then(viewProducts)
+    .then(viewLowInventory)
+    .then(addToInventory)
+    .then(addNewProduct)
+    .then(() => {
+      start();
+    })
+    .catch((err) => {
+      connection.end();
+      if (err === 'quit') {
+        console.log("Goodbye!")
+        return;
       }
-    }
-  }
 
-]).then(answers => {
-  console.log(JSON.stringify(answers, null ,' '));
-});
+      console.log("Captured an Error...");
+      console.log(err)
+    });
+
+}; //end start
+
+
+
+function managerView() {
+  var array = [
+    "View Products for Sale",
+    "View Low Inventory",
+    "Add to Inventory",
+    "Add New Product"
+  ];
+
+  function managerView() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'manage',
+        message: 'Manager View Menu: Pick a task.',
+        choices: array,
+        filter: function (val) {
+          switch (val) {
+            case "View Products for Sale":
+              return forSale;
+            case "View Low Inventory":
+              return lowInv;
+            case "Add to Inventory":
+              return addInv;
+            case "Add New Product":
+              return addProduct;
+            default:
+              console.log(`Error detected.`)
+          }
+        }
+      }
+
+    ]).then(answers => {
+      console.log(JSON.stringify(answers, null, ' '));
+    });
+  }
+}
